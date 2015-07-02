96c941668f4f622e9b9277ea5cabefffc04cc24f
oneriang <oneriang@126.com>
2015-07-03 01:50:51 +0800

    照片整理
    可以将指定路径（包含子文件夹）下的照片和视频，以拍摄日期时间重命名后，输出到指定路径。

---
+++ scr/zhaopianzhengli/zhaopianzhengli.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace zhaopianzhengli
+{
+    partial class zhaopianzhengli
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(zhaopianzhengli));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_qian = new System.Windows.Forms.Button();
+            this.txt_qian = new System.Windows.Forms.TextBox();
+            this.txt_hou = new System.Windows.Forms.TextBox();
+            this.btn_hou = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.fbd_qian = new System.Windows.Forms.FolderBrowserDialog();
+            this.fbd_hou = new System.Windows.Forms.FolderBrowserDialog();
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.bgw = new System.ComponentModel.BackgroundWorker();
+            this.btn_quxiao = new System.Windows.Forms.Button();
+            this.lbl_log = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择需要整理的文件夹:";
+            // 
+            // btn_qian
+            // 
+            this.btn_qian.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_qian.Location = new System.Drawing.Point(434, 41);
+            this.btn_qian.Name = "btn_qian";
+            this.btn_qian.Size = new System.Drawing.Size(75, 23);
+            this.btn_qian.TabIndex = 1;
+            this.btn_qian.Text = "选择";
+            this.btn_qian.UseVisualStyleBackColor = true;
+            this.btn_qian.Click += new System.EventHandler(this.btn_qian_Click);
+            // 
+            // txt_qian
+            // 
+            this.txt_qian.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_qian.Location = new System.Drawing.Point(16, 43);
+            this.txt_qian.Name = "txt_qian";
+            this.txt_qian.ReadOnly = true;
+            this.txt_qian.Size = new System.Drawing.Size(412, 21);
+            this.txt_qian.TabIndex = 2;
+            // 
+            // txt_hou
+            // 
+            this.txt_hou.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_hou.Location = new System.Drawing.Point(16, 86);
+            this.txt_hou.Name = "txt_hou";
+            this.txt_hou.ReadOnly = true;
+            this.txt_hou.Size = new System.Drawing.Size(412, 21);
+            this.txt_hou.TabIndex = 5;
+            // 
+            // btn_hou
+            // 
+            this.btn_hou.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_hou.Location = new System.Drawing.Point(434, 84);
+            this.btn_hou.Name = "btn_hou";
+            this.btn_hou.Size = new System.Drawing.Size(75, 23);
+            this.btn_hou.TabIndex = 4;
+            this.btn_hou.Text = "选择";
+            this.btn_hou.UseVisualStyleBackColor = true;
+            this.btn_hou.Click += new System.EventHandler(this.btn_hou_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "选择保存整理后照片的文件夹:";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_start.Location = new System.Drawing.Point(434, 129);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 6;
+            this.btn_start.Text = "开始";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // rtb
+            // 
+            this.rtb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb.Location = new System.Drawing.Point(16, 190);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(493, 186);
+            this.rtb.TabIndex = 7;
+            this.rtb.Text = "";
+            this.rtb.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // lbl
+            // 
+            this.lbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbl.Location = new System.Drawing.Point(16, 175);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(493, 12);
+            this.lbl.TabIndex = 8;
+            // 
+            // bgw
+            // 
+            this.bgw.WorkerReportsProgress = true;
+            this.bgw.WorkerSupportsCancellation = true;
+            this.bgw.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_DoWork);
+            this.bgw.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw_ProgressChanged);
+            this.bgw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw_RunWorkerCompleted);
+            // 
+            // btn_quxiao
+            // 
+            this.btn_quxiao.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_quxiao.Enabled = false;
+            this.btn_quxiao.Location = new System.Drawing.Point(353, 129);
+            this.btn_quxiao.Name = "btn_quxiao";
+            this.btn_quxiao.Size = new System.Drawing.Size(75, 23);
+            this.btn_quxiao.TabIndex = 9;
+            this.btn_quxiao.Text = "取消";
+            this.btn_quxiao.UseVisualStyleBackColor = true;
+            this.btn_quxiao.Click += new System.EventHandler(this.btn_quxiao_Click);
+            // 
+            // lbl_log
+            // 
+            this.lbl_log.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_log.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lbl_log.Location = new System.Drawing.Point(16, 159);
+            this.lbl_log.Name = "lbl_log";
+            this.lbl_log.Size = new System.Drawing.Size(493, 12);
+            this.lbl_log.TabIndex = 10;
+            // 
+            // zhaopianzhengli
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 388);
+            this.Controls.Add(this.lbl_log);
+            this.Controls.Add(this.btn_quxiao);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.rtb);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.txt_hou);
+            this.Controls.Add(this.btn_hou);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_qian);
+            this.Controls.Add(this.btn_qian);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "zhaopianzhengli";
+            this.Text = "照片整理";
+            this.Load += new System.EventHandler(this.zhaopianzhengli_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_qian;
+        private System.Windows.Forms.TextBox txt_qian;
+        private System.Windows.Forms.TextBox txt_hou;
+        private System.Windows.Forms.Button btn_hou;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.FolderBrowserDialog fbd_qian;
+        private System.Windows.Forms.FolderBrowserDialog fbd_hou;
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Label lbl;
+        private System.ComponentModel.BackgroundWorker bgw;
+        private System.Windows.Forms.Button btn_quxiao;
+        private System.Windows.Forms.Label lbl_log;
+    }
+}
+
---
+++ scr/zhaopianzhengli/zhaopianzhengli.cs
@@ -0,0 +1,303 @@
+﻿using MediaInfoNET;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace zhaopianzhengli
+{
+    public partial class zhaopianzhengli : Form
+    {
+        int cnt_photo = 0;
+        int cnt_video = 0;
+
+        public zhaopianzhengli()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_qian_Click(object sender, EventArgs e)
+        {
+            this.fbd_qian.ShowDialog();
+            this.txt_qian.Text = this.fbd_qian.SelectedPath;
+        }
+
+        private void btn_hou_Click(object sender, EventArgs e)
+        {
+            this.fbd_hou.ShowDialog();
+            this.txt_hou.Text = this.fbd_hou.SelectedPath;
+            string dir_qita = System.IO.Path.Combine(this.txt_hou.Text, "qita");
+            if (System.IO.Directory.Exists(dir_qita) == false)
+            {
+                System.IO.Directory.CreateDirectory(dir_qita);
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            bgw.RunWorkerAsync();
+            this.btn_quxiao.Enabled = true;
+            this.btn_start.Enabled = false;
+        }
+
+        private void check_dir(string path, string out_path)
+        {
+            string[] dirs = System.IO.Directory.GetDirectories(path);
+
+            foreach (string dir in dirs)
+            {
+                DirectoryInfo di = new DirectoryInfo(dir);
+
+                Boolean bl = true;
+                if ((di.Attributes & FileAttributes.System) == FileAttributes.System)
+                {
+                    bl = false;
+                }
+                if ((di.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+                {
+                    bl = false;
+                }
+
+                if (bl)
+                {
+                    Console.WriteLine(dir);
+                    check_dir(dir, out_path);
+                    check_file(dir, out_path);
+                }
+
+            }
+            
+            check_file(path, out_path);
+
+            System.IO.File.CreateText(System.IO.Path.Combine(path, DateTime.Now.ToString("yyyyMMddHHmmss") + "_copied.txt"));
+        }
+
+        private bool is_copy = false;
+        private string file_name_hou = "";
+        private string file_name_qian = "";
+
+        private void check_file(string path, string out_path)
+        {
+            try
+            {
+                System.Drawing.Bitmap bmp = null;
+                string val = "";
+                DateTime dt = new DateTime();
+                string file_name = "";
+                string dir_qita = System.IO.Path.Combine(out_path, "qita");
+                string file_name_qita = "";
+                string ext = "";
+                bool hasDate = false;
+                string[] files = System.IO.Directory.GetFiles(path);
+                string year = "";
+                string month = "";
+                string out_path_year = "";
+                string out_path_month = "";
+
+                foreach (string file in files)
+                {
+                    this.file_name_qian = file;
+                    bgw.ReportProgress(100);
+
+                    FileInfo fi = new FileInfo(file);
+                    Boolean bl = true;
+                    if ((fi.Attributes & FileAttributes.System) == FileAttributes.System)
+                    {
+                        bl = false;
+                    }
+                    if ((fi.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+                    {
+                        bl = false;
+                    }
+                    if(bl == false){
+                        continue;
+                    }
+                    Console.WriteLine(file);
+                    ext = System.IO.Path.GetExtension(file);
+                    if (ext.ToLower() == ".jpg")
+                    {
+                        //読み込む
+                        bmp = new System.Drawing.Bitmap(file);
+                        foreach (System.Drawing.Imaging.PropertyItem item in bmp.PropertyItems)
+                        {
+
+                            //Exif情報から撮影時間を取得する
+                            if (item.Id == 0x9003 && item.Type == 2)
+                            {
+                                //文字列に変換する
+                                val = System.Text.Encoding.ASCII.GetString(item.Value);
+                                val = val.Trim(new char[] { '\0' });
+
+                                try
+                                {
+                                    dt = DateTime.ParseExact(val, "yyyy:MM:dd HH:mm:ss", null);
+                                    
+                                    year = dt.Year.ToString();
+                                    out_path_year = System.IO.Path.Combine(out_path, year);
+                                    
+                                    if (System.IO.Directory.Exists(out_path_year) == false)
+                                    {
+                                        System.IO.Directory.CreateDirectory(out_path_year);
+                                    }
+
+                                    month = dt.Month.ToString();
+                                    out_path_month = System.IO.Path.Combine(out_path_year, month);
+
+                                    if (System.IO.Directory.Exists(out_path_month) == false)
+                                    {
+                                        System.IO.Directory.CreateDirectory(out_path_month);
+                                    }
+
+                                    file_name = dt.ToString("yyyy_MM_dd_HH_mm_ss");
+                                    file_name = System.IO.Path.Combine(out_path_month, file_name + ext);
+                                    Console.WriteLine(file_name);
+                                    if (System.IO.File.Exists(file_name) == false)
+                                    {
+                                        System.IO.File.Copy(file, file_name);
+                                        cnt_photo = cnt_photo + 1;
+                                        this.file_name_qian = file;
+                                        this.file_name_hou = file_name;
+                                        this.is_copy = true;
+                                        bgw.ReportProgress(100);
+                                    }
+                                }
+                                catch (Exception)
+                                {
+
+                                }
+
+                                hasDate = true;
+                                break;
+                            }
+                        }
+
+                        if (hasDate == false)
+                        {
+                            file_name_qita = System.IO.Path.GetFileName(file);
+                            file_name_qita = System.IO.Path.Combine(dir_qita, file_name_qita);
+                            if (System.IO.File.Exists(file_name_qita) == false)
+                            {
+                                System.IO.File.Copy(file, file_name_qita);
+                                cnt_photo = cnt_photo + 1;
+                                this.file_name_qian = file;
+                                this.file_name_hou = file_name_qita;
+                                this.is_copy = true;
+                                bgw.ReportProgress(100);
+                            }
+                        }
+
+                        bmp.Dispose();
+                    }
+
+                    if (ext.ToLower() == ".mp4" || ext.ToLower() == ".mov" || ext.ToLower() == ".3gp" || ext.ToLower() == ".wmv")
+                    {
+                        MediaFile mf = new MediaFile(file);
+                        foreach (MediaInfo_Stream ms in mf.AllStreams)
+                        {
+                            if (ms.Properties.ContainsKey("Encoded date"))
+                            {
+                                try
+                                {
+                                    String strDt = ms.Properties["Encoded date"];
+                                    strDt = strDt.Replace("UTC ", "");
+                                    dt = Convert.ToDateTime(strDt);
+
+                                    year = dt.Year.ToString();
+                                    out_path_year = System.IO.Path.Combine(out_path, year);
+
+                                    if (System.IO.Directory.Exists(out_path_year) == false)
+                                    {
+                                        System.IO.Directory.CreateDirectory(out_path_year);
+                                    }
+
+                                    month = dt.Month.ToString();
+                                    out_path_month = System.IO.Path.Combine(out_path_year, month);
+
+                                    if (System.IO.Directory.Exists(out_path_month) == false)
+                                    {
+                                        System.IO.Directory.CreateDirectory(out_path_month);
+                                    }
+
+                                    file_name = dt.ToString("yyyy_MM_dd_HH_mm_ss");
+                                    file_name = System.IO.Path.Combine(out_path_month, file_name + ext);
+                                    Console.WriteLine(file_name);
+                                    if (System.IO.File.Exists(file_name) == false)
+                                    {
+                                        System.IO.File.Copy(file, file_name);
+                                        cnt_video = cnt_video + 1;
+                                        this.file_name_qian = file;
+                                        this.file_name_hou = file_name;
+                                        this.is_copy = true;
+                                        bgw.ReportProgress(100);
+                                    }
+                                }
+                                catch (Exception)
+                                {
+                                }
+                                
+                            }
+                            if (ms.Properties.ContainsKey("Tagged date"))
+                            {
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.file_name_qian = "Error: " + ex.Message;
+                bgw.ReportProgress(100);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zhaopianzhengli_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            this.check_dir(this.txt_qian.Text, this.txt_hou.Text);
+        }
+
+        private void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.lbl_log.Text = "正在确认中文件： " + file_name_qian;
+
+            if(this.is_copy == true){
+                lbl.Text = "正在忙碌中... " + "已处理：" + cnt_photo.ToString() + "个照片。" + cnt_video.ToString() + "个视频。";
+                rtb.AppendText(file_name_qian);
+                rtb.AppendText("\r\n");
+                rtb.AppendText(file_name_hou);
+                rtb.AppendText("\r\n");
+            }
+            this.is_copy = false;
+            this.Refresh();
+        }
+
+        private void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lbl.Text = "已完成 ^-^ " + "全部处理：" + cnt_photo.ToString() + "个照片。" + cnt_video.ToString() + "个视频。";
+            MessageBox.Show("完成！");
+            this.btn_quxiao.Enabled = false;
+            this.btn_start.Enabled = true;
+        }
+
+        private void btn_quxiao_Click(object sender, EventArgs e)
+        {
+            if (bgw.WorkerSupportsCancellation == true)
+            {
+                bgw.CancelAsync();
+            }
+            lbl.Text = "中途取消 -_- " + "全部处理：" + cnt_photo.ToString() + "个照片。" + cnt_video.ToString() + "个视频。";
+            this.btn_quxiao.Enabled = false;
+            this.btn_start.Enabled = true;
+        }
+    }
+}
